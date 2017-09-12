fbb2ec34dce61637d9c9f40b32509e08fcdf2901
Tomosugi Tasaka <tuvy.ano@gmail.com>
2017-09-12 17:12:00 +0900

    refactor file name

--- app/src/main/java/com/tuvy/tomosugi/minimalpairs/view/MatchingListFragment.kt
+++ app/src/main/java/com/tuvy/tomosugi/minimalpairs/view/MatchingListFragment.kt
@@ -3,19 +3,18 @@ package com.tuvy.tomosugi.minimalpairs.view
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import com.tuvy.tomosugi.minimalpairs.MessageActivity
+import com.tuvy.tomosugi.minimalpairs.ChatActivity
 import com.tuvy.tomosugi.minimalpairs.R
 
 /**
  * Created by tomosugi on 2017/09/11.
  */
 
-class MessageListFragment : Fragment() {
+class MatchingListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.message_list, null)
     }
@@ -31,7 +30,7 @@ class MessageListFragment : Fragment() {
         testButton.setOnClickListener {
 
             val intent: Intent
-            intent = Intent(activity, MessageActivity::class.java)
+            intent = Intent(activity, ChatActivity::class.java)
 
             startActivity(intent)
         }
