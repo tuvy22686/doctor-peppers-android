49ba15b8ba6af48401aef092e03d2df0acd41333
x15071xx <x15071xx@aitech.ac.jp>
2017-09-12 15:51:07 +0900

    たたき台

--- app/src/main/java/com/tuvy/tomosugi/minimalpairs/view/MessageListFragment.kt
+++ app/src/main/java/com/tuvy/tomosugi/minimalpairs/view/MessageListFragment.kt
@@ -1,10 +1,14 @@
 package com.tuvy.tomosugi.minimalpairs.view
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import com.tuvy.tomosugi.minimalpairs.MessageActivity
 import com.tuvy.tomosugi.minimalpairs.R
 
 /**
@@ -14,5 +18,22 @@ import com.tuvy.tomosugi.minimalpairs.R
 class MessageListFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.message_list, null)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val testButton: Button
+        testButton = activity.findViewById(R.id.testButton) as Button
+
+        testButton.text = "test"
+
+        testButton.setOnClickListener {
+
+            val intent: Intent
+            intent = Intent(activity, MessageActivity::class.java)
+
+            startActivity(intent)
+        }
     }
 }
