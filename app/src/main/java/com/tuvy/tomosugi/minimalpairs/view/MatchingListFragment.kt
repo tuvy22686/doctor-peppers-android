84fb84d478dd6409e903c771241e93ba2f9e6852
Tomosugi Tasaka <tuvy.ano@gmail.com>
2017-09-13 12:12:57 +0900

    comp intent func

--- app/src/main/java/com/tuvy/tomosugi/minimalpairs/view/MatchingListFragment.kt
+++ app/src/main/java/com/tuvy/tomosugi/minimalpairs/view/MatchingListFragment.kt
@@ -8,8 +8,6 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import com.tuvy.tomosugi.minimalpairs.ChatActivity
 import com.tuvy.tomosugi.minimalpairs.R
 import com.tuvy.tomosugi.minimalpairs.controller.MatchingRecyclerViewAdapter
 import com.tuvy.tomosugi.minimalpairs.model.User
