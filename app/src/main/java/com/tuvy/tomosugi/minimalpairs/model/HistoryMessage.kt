ed207548664d2dd2ba697e16fd7bdc761177f936
Tomosugi Tasaka <tuvy.ano@gmail.com>
2017-09-14 16:56:13 +0900

    add api

--- app/src/main/java/com/tuvy/tomosugi/minimalpairs/model/HistoryMessage.kt
+++ app/src/main/java/com/tuvy/tomosugi/minimalpairs/model/HistoryMessage.kt
@@ -2,10 +2,12 @@ package com.tuvy.tomosugi.minimalpairs.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Message (
+/**
+ * Created by tomosugi on 2017/09/14.
+ */
+
+data class HistoryMessage (
         @SerializedName("user_id") val userId: Int,
         @SerializedName("partner_id") val partnerId: Int,
-        @SerializedName("from_me") val fromMe: Int,
-        @SerializedName("content") val text: String,
-        @SerializedName("created_at") val timestamp: Int
+        @SerializedName("messages") val messages: List<Message>
 )
