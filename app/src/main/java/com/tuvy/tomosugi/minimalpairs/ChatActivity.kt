10ca94e21ca59c100c7256ac81472202cc691320
Naoyuki Sugi <bnsgny@gmail.com>
2017-09-14 12:05:40 +0900

    toolbarにバックボタンとオプションボタンを実装

--- app/src/main/java/com/tuvy/tomosugi/minimalpairs/ChatActivity.kt
+++ app/src/main/java/com/tuvy/tomosugi/minimalpairs/ChatActivity.kt
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import android.widget.Toolbar
 import com.tuvy.tomosugi.minimalpairs.controller.ChatRecyclerViewAdapter
 import com.tuvy.tomosugi.minimalpairs.model.Message
@@ -16,21 +19,39 @@ class ChatActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
 
-//        val toolbar: android.support.v7.widget.Toolbar = findViewById(R.id.action_bar) as android.support.v7.widget.Toolbar
-//        setSupportActionBar(toolbar)
-//
-//        supportActionBar?.setDefaultDisplayHomeAsUpEnabled(true)
-//        supportActionBar?.setHomeButtonEnabled(true)
+        val toolbar: android.support.v7.widget.Toolbar = findViewById(R.id.toolbar) as android.support.v7.widget.Toolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+        supportActionBar?.title = "partnerName"
+
+        toolbar.setNavigationOnClickListener {
+            onBackPressed()
+        }
 
         val rv = findViewById(R.id.massageRecyclewView) as RecyclerView
         val llm = LinearLayoutManager(this)
 
 //        val data: List<Message> = List(3, { index -> Message(index.toString())})
-        val data: List<Message> = List(3, { index -> Message(partnerName = "太郎" , message = "へけっ\nへけっ\nへけっ\nへけっ\nへけっ\nへけっ")})
+        val data: List<Message> = List(3, { index -> Message(partnerName = "太郎", message = "へけっ\nへけっ\nへけっ\nへけっ\nへけっ\nへけっ") })
 
         rv.setHasFixedSize(true)
         rv.layoutManager = llm
         rv.adapter = ChatRecyclerViewAdapter(data)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_chat, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        var id: Int = item!!.itemId
+        if (id == R.id.action_setting) {
+//            Toast.makeText(this, "設定", Toast.LENGTH_LONG).show();
+            return true
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
