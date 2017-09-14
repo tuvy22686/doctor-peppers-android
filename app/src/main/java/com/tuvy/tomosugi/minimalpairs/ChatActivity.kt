6638344ce1a92cf212502933ce77136c18b22b3e
Naoyuki Sugi <bnsgny@gmail.com>
2017-09-15 00:54:33 +0900

    初めての会話の画面を雑に作成

--- app/src/main/java/com/tuvy/tomosugi/minimalpairs/ChatActivity.kt
+++ app/src/main/java/com/tuvy/tomosugi/minimalpairs/ChatActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Matrix
 import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.Drawable
 import android.graphics.drawable.ScaleDrawable
+import android.media.Image
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.content.res.ResourcesCompat
@@ -14,51 +15,91 @@ import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.LinearLayout
-import android.widget.TextView
-import android.widget.Toast
-import android.widget.Toolbar
+import android.widget.*
 import com.tuvy.tomosugi.minimalpairs.controller.ChatRecyclerViewAdapter
 import com.tuvy.tomosugi.minimalpairs.model.Message
+import rm.com.longpresspopup.LongPressPopup
+import rm.com.longpresspopup.LongPressPopupBuilder
 
 class ChatActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat)
 
-        val toolbar: android.support.v7.widget.Toolbar = findViewById(R.id.toolbar) as android.support.v7.widget.Toolbar
-        setSupportActionBar(toolbar)
+        //Congratuations画面
+        setContentView(R.layout.congratulations)
 
-        var intent = intent
-        var partnerId: Int = intent.getIntExtra("partnerId", -1)
-        var partnerName = when(partnerId) {
-            0 -> "零マン"
-            1 -> "おとこ"
-            2 -> "おんな"
-            else -> "null子"
+        var kimochiButton: Button = findViewById(R.id.kimochi_button) as Button
+        var cancelButton: ImageButton = findViewById(R.id.cancel_button) as ImageButton
+
+        //「気持ちを伝える」ボタンを押して遷移
+        kimochiButton.setOnClickListener {
+            setContentView(R.layout.activity_chat_first)
+
+            var popup = LongPressPopupBuilder(this)
+                    .setTarget(btn)
+                    //.setPopupView(textView)// Not using this time
+                    .setPopupView(R.layout.popup_layout, this)
+                    .setLongPressDuration(750)
+                    .setDismissOnLongPressStop(false)
+                    .setDismissOnTouchOutside(false)
+                    .setDismissOnBackPressed(false)
+                    .setCancelTouchOnDragOutsideView(true)
+                    .setLongPressReleaseListener(this)
+                    .setOnHoverListener(this)
+                    .setPopupListener(this)
+                    .setTag("PopupFoo")
+                    .setAnimationType(LongPressPopup.ANIMATION_TYPE_FROM_CENTER)
+                    .build();
+
+            onViewIn
+
+
         }
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        supportActionBar?.setDisplayShowHomeEnabled(true)
-        supportActionBar?.setHomeAsUpIndicator(resizeDrawable(resources.getDrawable(R.drawable.arrow)))
-        supportActionBar?.title = partnerName
-        toolbar.setNavigationOnClickListener {
-            onBackPressed()
-        }
 
-        if (intent != null) {
-            Log.d("onCreate", intent.getIntExtra("partnerId", -1).toString())
-        }
 
-        val rv = findViewById(R.id.massageRecyclewView) as RecyclerView
-        val llm = LinearLayoutManager(this)
+
+
+        //「キャンセル」ボタンを押して遷移
+        cancelButton.setOnClickListener {
+            setContentView(R.layout.activity_chat)
+
+            val toolbar: android.support.v7.widget.Toolbar = findViewById(R.id.toolbar) as android.support.v7.widget.Toolbar
+            setSupportActionBar(toolbar)
+
+            var intent = intent
+            var partnerId: Int = intent.getIntExtra("partnerId", -1)
+            var partnerName = when (partnerId) {
+                0 -> "零マン"
+                1 -> "おとこ"
+                2 -> "おんな"
+                else -> "null子"
+            }
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            supportActionBar?.setDisplayShowHomeEnabled(true)
+            supportActionBar?.setHomeAsUpIndicator(resizeDrawable(resources.getDrawable(R.drawable.arrow)))
+            supportActionBar?.title = partnerName
+            toolbar.setNavigationOnClickListener {
+                onBackPressed()
+            }
+
+            if (intent != null) {
+                Log.d("onCreate", intent.getIntExtra("partnerId", -1).toString())
+            }
+
+            val rv = findViewById(R.id.massageRecyclewView) as RecyclerView
+            val llm = LinearLayoutManager(this)
 
 //        val data: List<Message> = List(3, { index -> Message(index.toString())})
-        val data: List<Message> = List(3, { timestamp -> Message(partnerId = 1, userId = 2, fromMe = 1, timestamp = timestamp, text = "hello")})
+            val data: List<Message> = List(3, { timestamp -> Message(partnerId = 1, userId = 2, fromMe = 1, timestamp = timestamp, text = "hello") })
 
-        rv.setHasFixedSize(true)
-        rv.layoutManager = llm
-        rv.adapter = ChatRecyclerViewAdapter(data)
+            rv.setHasFixedSize(true)
+            rv.layoutManager = llm
+            rv.adapter = ChatRecyclerViewAdapter(data)
+
+
+        }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
