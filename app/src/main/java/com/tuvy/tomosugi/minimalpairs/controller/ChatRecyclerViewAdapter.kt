a01cf3ebfdb92a76d54e401d0838a6e96390962d
Naoyuki Sugi <bnsgny@gmail.com>
2017-09-14 18:21:11 +0900

    update

--- app/src/main/java/com/tuvy/tomosugi/minimalpairs/controller/ChatRecyclerViewAdapter.kt
+++ app/src/main/java/com/tuvy/tomosugi/minimalpairs/controller/ChatRecyclerViewAdapter.kt
@@ -26,7 +26,7 @@ class ChatRecyclerViewAdapter(private val data: List<Message>) : RecyclerView.Ad
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         holder as MessageViewHolder
-        holder.messageView.text = data[position].message
+        holder.messageView.text = data[position].text
 
         Glide
                 .with(holder.partnerIcon.context)
