ec3b602ca2440101f2598245763e8de79e85575f
Tomosugi Tasaka <tuvy.ano@gmail.com>
2017-09-15 12:46:44 +0900

    add getting user information

--- app/src/main/java/com/tuvy/tomosugi/minimalpairs/MainActivity.kt
+++ app/src/main/java/com/tuvy/tomosugi/minimalpairs/MainActivity.kt
@@ -54,22 +54,22 @@ class MainActivity : AppCompatActivity() {
 //                    Log.d("getProfile", it.name)
 //                }
 
-        //会話履歴取得用API(テスト)
-        client.history(userId = 1, partnerId = 2)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe {
-                    Log.d("history", "doOnSubscribe")
-                }
-                .doOnError {
-                    Log.d("history", "doOnError")
-                }
-                .doOnNext {
-                    Log.d("history", "doOnNext")
-                }
-                .subscribe {
-                    Log.d("history", "subscribe")
-                }
+//        //会話履歴取得用API(テスト)
+//        client.history(userId = 1, partnerId = 2)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .doOnSubscribe {
+//                    Log.d("history", "doOnSubscribe")
+//                }
+//                .doOnError {
+//                    Log.d("history", "doOnError")
+//                }
+//                .doOnNext {
+//                    Log.d("history", "doOnNext")
+//                }
+//                .subscribe {
+//                    Log.d("history", "subscribe")
+//                }
 
 
         setContentView(R.layout.activity_main)
